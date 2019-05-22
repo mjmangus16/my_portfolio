8b7c7bcb22dfbd58536a825ba247dce4841959e3
MMangus <mjmangus16@yahoo.com>
2019-05-22 14:22:38 -0400

    php bugs

--- contact.php
+++ contact.php
@@ -17,11 +17,11 @@ $to = "mjmangus@gmail.com";
 
 $headers = "From: $email_from \r\n";
 
-$headers .= "Reply To: $visitor_email \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location: ./index.html");
+header("Location: index.html");
 
 
 
